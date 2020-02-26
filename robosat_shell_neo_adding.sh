e5bbbc8e5c61beb3614176030db70638962fe0ed
Your Name <you@example.com>
2020-02-26 15:31:35 +0530

    v6

--- robosat_shell_neo_adding.sh
+++ robosat_shell_neo_adding.sh
@@ -7,9 +7,7 @@ MODEL="model"
 TRAIN="train"
 TEST="test"
 PREDICT="predict"
-EPOCHS=10
-zoom=20
-batch=1
+EPOCHS=5
 
 
 function config() {
@@ -22,7 +20,7 @@ function config() {
 function clean() {
 	echo "----- cleaning -----"
 	rm -rf $PRE
-	mkdir -p $PRE/tif $PRE/geojson $PRE/urls $PRE/train
+	mkdir -p $PRE/tif $PRE/geojson $PRE/urls 
 }
 
 
@@ -55,29 +53,34 @@ function train_download_tif_geojson() {
 # tif image to tiles
 function train_tile() {
 	echo "----- training tile to image -------"
-	neo tile --zoom $zoom --ts 1024,1024 --bands 1,2,3 --nodata_threshold 25 --rasters $PRE/tif/*.tif --out $PRE/images
+	neo tile --zoom 20 --ts 1024,1024 --nodata_threshold 25 --rasters $PRE/tif/*.tif --out $PRE/images
 	neo cover --dir $PRE/images --out $PRE/images/cover.csv
-	echo "--------- resterise -------------"
-	neo rasterize --config=tanzania.toml --ts 1024,1024 --geojson $PRE/geojson/*.geojson --type Building --cover $PRE/images/cover.csv --out $PRE/labels
-	# neo tile --zoom 19 --bands 1,2,3 --nodata_threshold 25 --rasters train/*/*[^-]/*tif --out train/images
+
+	for geoJson in `ls $PRE/geojson/*.geojson | head -$top_n`
+	do
+	  ogr2ogr -f SQLite $PRE/tanzania_labels.sqlite  $geoJson -dsco SPATIALITE=YES -t_srs EPSG:32630 -nlt PROMOTE_TO_MULTI -nln building -lco GEOMETRY_NAME=geom -append 
+	done
+
+	ogr2ogr -f GeoJSON $PRE/building.json $PRE/tanzania_labels.sqlite -dialect sqlite -sql "SELECT Buffer(geom, -0.25) AS geom FROM building" 
+	neo rasterize --ts 1024,1024 --geojson $PRE/building.json --config=tanzania.toml --type Building --cover $PRE/images/cover.csv --out $PRE/labels
 }
 
 
 
-# function train_set() {
-# 	echo "----- training set -------"
-# 	# Create Training DataSet
-# 	awk '$2 > 0 { print $1 }' $PRE/labels/building_cover.csv > $PRE/buildings_cover.csv
-# 	awk '$2 == 0 { print $1 }' $PRE/labels/building_cover.csv > $PRE/no_building_cover.csv
-# 	sort -R $PRE/no_building_cover.csv | head -n 5000 > $PRE/no_building_subset_cover.csv
-# 	cat $PRE/buildings_cover.csv $PRE/no_building_subset_cover.csv > $PRE/cover.csv
+function train_set() {
+	echo "----- training set -------"
+	# Create Training DataSet
+	awk '$2 > 0 { print $1 }' $PRE/labels/building_cover.csv > $PRE/buildings_cover.csv
+	awk '$2 == 0 { print $1 }' $PRE/labels/building_cover.csv > $PRE/no_building_cover.csv
+	sort -R $PRE/no_building_cover.csv | head -n 5000 > $PRE/no_building_subset_cover.csv
+	cat $PRE/buildings_cover.csv $PRE/no_building_subset_cover.csv > $PRE/cover.csv
 
-# 	neo cover --cover $PRE/cover.csv --splits 90/10 --out $PRE/train/cover.csv $PRE/eval/cover.csv
-# 	neo subset --dir $PRE/images --cover $PRE/train/cover.csv --out $PRE/train/images
-# 	neo subset --dir $PRE/labels --cover $PRE/train/cover.csv --out $PRE/train/labels
-# 	neo subset --dir $PRE/images --cover $PRE/eval/cover.csv --out $PRE/eval/images
-# 	neo subset --dir $PRE/labels --cover $PRE/eval/cover.csv --out $PRE/eval/labels
-# }
+	neo cover --cover $PRE/cover.csv --splits 90/10 --out $PRE/train/cover.csv $PRE/eval/cover.csv
+	neo subset --dir $PRE/images --cover $PRE/train/cover.csv --out $PRE/train/images
+	neo subset --dir $PRE/labels --cover $PRE/train/cover.csv --out $PRE/train/labels
+	neo subset --dir $PRE/images --cover $PRE/eval/cover.csv --out $PRE/eval/images
+	neo subset --dir $PRE/labels --cover $PRE/eval/cover.csv --out $PRE/eval/labels
+}
 
 
 
@@ -86,7 +89,7 @@ function preprocessing() {
 	# clean
 	train_download_tif_geojson $1 $2
 	train_tile
-	# train_set 
+	train_set 
 }
 
 
@@ -123,13 +126,12 @@ function train() {
 	    count=`ls "${MODEL}"/*.pth | sort | wc -l`
 		epochs=${latest_checkpoint//[!0-9]/}    #extract numver from string
       	epochs=$((10#$epochs +EPOCHS))      # add other new epochs
-      	neo train --config=tanzania.toml --resume --checkpoint="${latest_checkpoint}" --dataset $PRE --epochs $epochs --out "${MODEL}" --bs=1
-		neo eval --config=tanzania.toml --checkpoint "${latest_checkpoint}" --dataset $PRE
+      	neo train --config=tanzania.toml --resume --checkpoint="${latest_checkpoint}" --dataset $PRE/train --epochs $epochs --out "${MODEL}" --bs=1
+		neo eval --checkpoint "${latest_checkpoint}" --dataset $PRE/train
 	else
       	echo "----- new checkpoints ------------"
-	    latest_checkpoint=`ls "${MODEL}"/*.pth | sort | tail -n -1`
-	    neo train --config=tanzania.toml --dataset $PRE  --epochs $EPOCHS --out "${MODEL}" --bs=1
-	    neo eval --config=tanzania.toml --checkpoint "${latest_checkpoint}" --dataset $PRE
+	    neo train --config=tanzania.toml --dataset $PRE/train  --epochs 5 --out "${MODEL}" --bs=1
+	    neo eval --checkpoint `ls "${MODEL}"/*.pth | sort | tail -n -1` --dataset $PRE/train
 	fi
 }
 
@@ -146,7 +148,7 @@ test_download() {
 
 test_tile() {
 	echo "-----------test split tiles ----------------"
-	neo tile --zoom $zoom  --nodata_threshold 25 --rasters $TEST/tif/*tif --out $PREDICT/images
+	neo tile --zoom 20  --nodata_threshold 25 --rasters $TEST/tif/*tif --out $PREDICT/images
 	neo cover --dir $PREDICT/images --out $PREDICT/images/cover.csv
 }
 
